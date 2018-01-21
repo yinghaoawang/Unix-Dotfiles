7908097495a51f94c154c25f7b48a5c6665b5e89
Yinghao Wang <yinghao.a.wang@gmail.com>
2018-01-21 15:49:36 -0800

    fix not install tmux config

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ rm -f ~/vimrc
 cp -r .vim ~
 rm -rf ~/.tmux
 cp -r .tmux ~
-cp .tmux.conf .
+cp .tmux.conf ~
 
 sudo git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
